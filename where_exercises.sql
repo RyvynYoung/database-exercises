5be49f6a0fb16f1074df7c744a8d2d817f2313c9
RyvynYoung <ryvyn.young@gmail.com>
2020-07-21 12:14:31 -0500

    add where_exercises

---
+++ where_exercises.sql
@@ -0,0 +1,49 @@
+--
+use employees;
+
+-- 709 rows
+SELECT * from employees
+where first_name in ('Irena', 'Vidya', 'Maya');
+
+-- 7330 rows
+SELECT * from employees
+where last_name like ('E%');
+
+-- 135214 rows
+select * from employees
+where hire_date like ('199%');
+
+-- 842 rows
+select * from employees
+where birth_date like ('%-12-25');
+
+-- 1873 rows
+SELECT * from employees
+where last_name like ('%q%');
+
+
+-- 709 rows use OR
+SELECT * from employees
+where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
+
+-- 441 rows use or
+SELECT * from employees
+where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
+and gender = 'M';
+
+-- 30723 rows
+SELECT * from employees
+where last_name like ('E%') or last_name like ('%e')
+
+-- 899 rows
+SELECT * from employees
+where last_name like ('E%') and last_name like ('%e')
+
+-- 362 rows
+select * from employees
+where hire_date like ('199%') and birth_date like ('%-12-25')
+
+-- 547 rows
+SELECT * from employees
+where last_name like ('%q%') and not last_name like ('%qu%')
